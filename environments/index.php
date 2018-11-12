d61356d6c23ab2e2299feafe9f7088a1d69e20b7
li <lizhong43403@163.com>
2018-11-12 16:55:13 +0800

    新增线上环境配置及微信验证所需文件

--- environments/index.php
+++ environments/index.php
@@ -46,20 +46,20 @@ return [
             'frontend/config/main-local.php',
         ],
     ],
-    // 'Production' => [
-    //     'path' => 'prod',
-    //     'setWritable' => [
-    //         'backend/runtime',
-    //         'backend/web/assets',
-    //         'frontend/runtime',
-    //         'frontend/web/assets',
-    //     ],
-    //     'setExecutable' => [
-    //         'yii',
-    //     ],
-    //     'setCookieValidationKey' => [
-    //         'backend/config/main-local.php',
-    //         'frontend/config/main-local.php',
-    //     ],
-    // ],
+    'Production' => [
+        'path' => 'prod',
+        'setWritable' => [
+            'backend/runtime',
+            'backend/web/assets',
+            'frontend/runtime',
+            'frontend/web/assets',
+        ],
+        'setExecutable' => [
+            'yii',
+        ],
+        'setCookieValidationKey' => [
+            'backend/config/main-local.php',
+            'frontend/config/main-local.php',
+        ],
+    ],
 ];
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=bdm275410125.my3w.com;dbname=bdm275410125_db',
+            'username' => 'bdm275410125',
+            'password' => 'Woaini123bd',
             'charset' => 'utf8',
         ],
         'mailer' => [
